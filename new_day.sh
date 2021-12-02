119bea7feda0fdf4b778ef0c25814cb039a12421
Nils Jakobson Mo <60095619+nilsmo1@users.noreply.github.com>
2021-12-02 16:33:41 +0100

    Delete new_day.sh

--- new_day.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-mkdir day$1; cd day$1
-touch sample;
-curl -o puzzle-input -b 'session=53616c7465645f5f67b0648410072707c03e88323408112029a68cdeaf6875df04a1b4209297c3b111320bc960a15bfb' https://adventofcode.com/2021/day/$1/input
-cp ../template $1.py
