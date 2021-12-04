dd380a17f3a8e7fd53996fb1a4398e18c5d88488
Nils <nilsmo1@hotmail.com>
2021-12-04 18:26:26 +0100

    Day 4 Complete!

--- day4/4.py
+++ day4/4.py
@@ -1,43 +1,83 @@
 # Day 4, Giant Squid
 
 from typing import List
+Board = List[List[int]]
 
 class BingoBoard:
-    def __init__(self, layout: List[List[int]]) -> None:
-        self.layout = layout
-        self.shown = []
-    
-    def add_to_shown(self, num: int) -> None:
-        self.shown.append(num)
-    
-    @property
-    def winner(self) -> bool:
-        pass
+    def __init__(self, layout: Board) -> None:
+        self.layout = [row for row in layout if row]
+        self.bingo_flag = False
 
     @property
-    def final_score(self) -> int:
-        return sum(x for x in line for line in self.layout if x not in self.shown)
+    def get_bingo_flag(self):
+        return self.bingo_flag
 
-#def make_BingoBoard(layout: 
+    def set_bingo_flag(self):
+        self.bingo_flag = True
+
+    def winner(self, shown: List[int]) -> bool:
+        cols  = [[row[i] for row in self.layout] for i in range(len(self.layout[0]))]
+        rows  = self.layout
+        for row, col in zip(cols, rows):
+            if all(e in shown for e in row) or all(e in shown for e in col):
+                return True
+
+    def final_score(self, shown: List[int]) -> int:
+        return shown[-1] * sum(sum(x for x in line if x not in shown) for line in self.layout)
+
+def make_BingoBoards(layout: List[Board]) -> BingoBoard:
+    return [BingoBoard(board) for board in layout]
 
 # Q1
-
+def run_bingo(numbers: List[int], boards: List[BingoBoard]) -> int:
+    shown = numbers[:4]
+    for number in numbers[4:]:
+        shown.append(number)
+        for board in boards:
+            if board.winner(shown):
+                return board.final_score(shown)
+    raise ValueError('No winner!')
 
 # Q2
+def run_bingo_last_board(numbers: List[int], boards: List[BingoBoard]) -> int:
+    num_winners_left = len(boards)
+    shown = numbers[:4]
+    for number in numbers[4:]:
+        shown.append(number)
+        for board in boards:
+            if board.winner(shown) and not board.get_bingo_flag:
+                num_winners_left -= 1
+                board.set_bingo_flag()
+                if not num_winners_left:
+                    return board.final_score(shown)
+    raise ValueError('No winner!')
 
 if __name__ == '__main__':
     # Samples
     with open('sample', 'r') as sample:
-        sample_input   = sample.read().split('\n')
-    sample_numbers = sample_input[0]
-    sample_boards  = [line for line in sample_input[1:] if line]
+        sample_input   = sample.read().split('\n\n')
+    sample_numbers = [int(e) for e in sample_input[0].split(',')]
+    sample_boards  = [list(map(
+                      lambda x: list(map(int, x.split())),
+                      line.split('\n')))
+                      for line in sample_input[1:] if line]
+    sample_BingoBoards = make_BingoBoards(sample_boards)
     
     # Tests
+    assert run_bingo(sample_numbers, sample_BingoBoards) == 4512
     
+    assert run_bingo_last_board(sample_numbers, sample_BingoBoards) == 1924
+
     # Puzzle input
-    #with open('puzzle-input', 'r') as RAW:
-    #    formatted = [int(line.strip()) for line in RAW] 
-    
+    with open('puzzle-input', 'r') as RAW:
+        formatted = RAW.read().split('\n\n')
+    formatted_numbers = [int(e) for e in formatted[0].split(',')]
+    formatted_boards  = [list(map(
+                         lambda x: list(map(int, x.split())), 
+                         line.split('\n'))) 
+                         for line in formatted[1:] if line]
+    formatted_BingoBoards = make_BingoBoards(formatted_boards)
+
     # Results
-    print(f'Q1: ')
-    print(f'Q2: ')
+    print(f'Q1: {run_bingo(formatted_numbers, formatted_BingoBoards)}')
+    print(f'Q2: {run_bingo_last_board(formatted_numbers, formatted_BingoBoards)}')
