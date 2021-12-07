71f4e8dae6154a99df42e9a6b94d7054ddc0676e
nilsmo1 <nilsmo1@hotmail.com>
2021-12-07 17:51:08 +0100

    Added a README

--- NEWDAY.sh
+++ NEWDAY.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 mkdir day$1; cd day$1
 touch sample;
-curl -o puzzle-input -b 'session=' https://adventofcode.com/2021/day/$1/input
+curl -o puzzle-input -b 'session=?' https://adventofcode.com/2021/day/$1/input
 cp ../template $1.py
