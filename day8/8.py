259679c8fd9bdd3769438a1156a69bc04e894f55
nilsmo1 <nilsmo1@hotmail.com>
2021-12-08 19:18:45 +0100

    Day 8, part 2 Complete! Added a file with my thoughts

--- day8/8.py
+++ day8/8.py
@@ -12,6 +12,37 @@ def count_uniques(words: List[In_Out]) -> int:
     return total
 
 # Q2
+def figure_stuff_out(word: In_Out) -> int:
+    deduction = {}
+    for s in word[0]:
+        if   len(s) == 2: deduction[1] = s
+        elif len(s) == 3: deduction[7] = s
+        elif len(s) == 4: deduction[4] = s
+        elif len(s) == 7: deduction[8] = s
+
+    for s in word[0]:
+        if len(s) == 5:
+            if all([c in s for c in deduction[7]]):
+                deduction[3] = s
+            elif all([c in s for c in deduction[4] if c not in deduction[1]]):
+                deduction[5] = s
+            else: 
+                deduction[2] = s
+
+    for s in word[0]:
+        if len(s) == 6:
+            if not all([c in s for c in deduction[1]]):
+                deduction[6] = s
+            elif all([c in s for c in deduction[4] if c not in deduction[1]]):
+                deduction[9] = s
+            else:
+                deduction[0] = s
+
+    out = int(''.join([str(i) for x in word[1] for i in range(10) if sorted(x) == sorted(deduction[i])]))
+    return out
+
+def sum_results(rows: List[In_Out]) -> int:
+    return sum(figure_stuff_out(row) for row in rows)
 
 if __name__ == '__main__':
     # Samples
@@ -22,6 +53,8 @@ if __name__ == '__main__':
     # Tests
     assert count_uniques(sample_words) == 26
 
+    assert sum_results(sample_words) == 61229
+
     # Puzzle input
     with open('puzzle-input', 'r') as RAW:
         formatted = [line.strip().split(' | ') for line in RAW.readlines()]
@@ -29,4 +62,4 @@ if __name__ == '__main__':
     
     # Results
     print(f'Q1: {count_uniques(formatted_words)}')
-    print(f'Q2: ')
+    print(f'Q2: {sum_results(formatted_words)}')
