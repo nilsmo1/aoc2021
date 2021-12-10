624808c0520aee7fff8e76b575f64559acc2e426
nilsmo1 <nilsmo1@hotmail.com>
2021-12-10 08:35:50 +0100

    Changed some things in NEWDAY.sh, and also the format of the folder- and filenames.

--- NEWDAY.sh
+++ NEWDAY.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-mkdir day$1; cd day$1
+if [ $1 -lt 10 ]; then
+    D="0$1";
+else
+    D="$1";
+fi
+mkdir day$D; cd day$D
 touch sample;
+cp ../template $D.py
 curl -o puzzle-input -b 'session=?' https://adventofcode.com/2021/day/$1/input
-cp ../template $1.py
